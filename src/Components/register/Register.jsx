1fcd014c0aa9f0a0191975527210ee4b76adeaba
tasib94 <rahi84974@gmail.com>
2025-07-19 12:18:28 +0600

    event handler added to the form successfully

--- src/Components/register/Register.jsx
+++ src/Components/register/Register.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
 const Register = () => {
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        console.log(event.target.email.value)
+    }
+
     return (
         <div>
             <h3 className="text-3xl">Register</h3>
+            <form onSubmit={handleRegister}>
+                <div className="hero bg-base-200 min-h-screen">
+                    <div className="hero-content flex-col ">
+                        <div className="text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">Register now!</h1>
+
+                        </div>
+                        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                            <div className="card-body">
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" name="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button className="btn btn-neutral mt-4">Login</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </form>
         </div>
     );
 };
