63b9425a46ab3c94cfbc6425665b802f54b25746
tasib94 <rahi84974@gmail.com>
2025-07-19 11:46:47 +0600

    route setup and header component setup done

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Main from './layout/Main.jsx';
+import Header from './Components/header/Header.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/header/home/Home.jsx';
+import Login from './Components/login/Login.jsx';
+import Register from './Components/register/Register.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path: "/login",
+        element:<Login></Login>
+      },
+      {
+        path: "register",
+        element: <Register></Register>
+      }
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
